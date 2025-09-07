2a092fe38fb549e16c79371517383ba9fb003997
SVITLANA OSTAPIUK <chyvacok188@gmail.com>
2025-09-07 11:06:57 +0200

    modified

--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -2,18 +2,15 @@
 import axios from "axios";
 import type { Movie } from "../types/movie";
 
-// Інтерфейс для відповіді від TMDB API
 interface TmdbResponse {
     results: Movie[];
     total_pages: number;
 }
 
-// Створюємо екземпляр axios з базовими налаштуваннями
 const apiClient = axios.create({
     baseURL: "https://api.themoviedb.org/3",
     headers: {
         Authorization: `Bearer ${import.meta.env.VITE_TMDB_TOKEN}`,
-        // Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzM2Q5ZDk1MTIzOTNmYzFlZjE4Njk3YjE5OGY0YmUxZCIsIm5iZiI6MTc1NzAwMjM5NS4wNjgsInN1YiI6IjY4YjliYTliZDgyNmM3ZDUwOTA2OGY1MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ZhyXrJohbo24lgL5KZoGiZfS-5WeQQvLW5csHpZuxss`,
     },
 });
 
